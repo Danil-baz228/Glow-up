e71eb7e8de5f0c4713e65cfc92bcb0d0825a450f
Anton Shimeiko <shumejko.sasha@gmail.com>
2024-09-26 20:53:51 +0300

    completed authController

--- react-diagram/src/components/AuthComponent.js
+++ react-diagram/src/components/AuthComponent.js
@@ -4,11 +4,17 @@ import appleIcon from './img/AuthController/icon_apple.png';
 import facebookIcon from './img/AuthController/icon_facebook.png';
 import googleIcon from './img/AuthController/icon_google.png';
 import emailIcon from './img/AuthController/icon_email.png';
+import eyeIcon from './img/AuthController/icon_eye.png';
 
 
 const AuthComponent = () => {
     const [authState, setAuthState] = useState('socialNetworksRegistration'); // Can be 'signup', 'login', or 'passwordRecovery'
     const [isModalOpen, setIsModalOpen] = useState(true);
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setIsPasswordVisible(!isPasswordVisible);
+    }
 
     const renderForm = () => {
         switch (authState) {
@@ -76,7 +82,11 @@ const AuthComponent = () => {
                             <input type="tel" placeholder="Phone"/>
                             <input type="email" placeholder="E-mail*" required/>
                             <input type="date" placeholder="Date of birth"/>
-                            <input type="password" placeholder="Password*" required/>
+                            <div className="password-input-wrapper">
+                                <input type={isPasswordVisible ? "text" : "password"} placeholder="Password*" required/>
+                                <img src={eyeIcon} alt="Show Password" onClick={togglePasswordVisibility}
+                                     className="eye-icon"/>
+                            </div>
                             <input type="password" placeholder="Password confirmation*" required/>
                             <div className="checkbox-wrapper-4">
                                 <input className="inp-cbx" id="terms" type="checkbox" required/>
@@ -108,16 +118,37 @@ const AuthComponent = () => {
                         <form>
                             <input type="email" placeholder="E-mail*" required/>
                             <input type="password" placeholder="Password*" required/>
+                            <div className="checkbox-wrapper-4">
+                                <input className="inp-cbx" id="terms" type="checkbox" required/>
+                                <label className="cbx" htmlFor="terms">
+                                    <span>
+                                        <svg width="12px" height="10px">
+                                        <use xlinkHref="#check-4"></use>
+                                        </svg>
+                                    </span>
+                                    <span>Remember me</span>
+                                </label>
+                                <svg className="inline-svg">
+                                    <symbol id="check-4" viewBox="0 0 12 10">
+                                        <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                                    </symbol>
+                                </svg>
+                            </div>
                             <button type="submit">Log In</button>
                         </form>
                     </div>
                 );
             case 'passwordRecovery':
                 return (
-                    <form>
-                        <input type="email" placeholder="E-mail*" required/>
-                        <button type="submit">Recover Password</button>
-                    </form>
+                    <div className={"passwordRecoveryBox"}>
+                        <h2>Password recovery</h2>
+                        <p>Forgot your password?</p>
+                        <p>Enter your E-mail to get a password or create a new one.</p>
+                        <form>
+                            <input type="email" placeholder="E-mail*" required/>
+                            <button type="submit">Recover Password</button>
+                        </form>
+                    </div>
                 );
             default:
                 return null;
@@ -133,27 +164,39 @@ const AuthComponent = () => {
             </div>
 
             <div className="auth-footer">
-                <p>
-                    {
+            {
                         authState === 'socialNetworksRegistration' || authState === 'socialNetworksLogin' ? (
                             <div className={"termsInfo"}>By continuing, you agree to the privacy policy</div>
                         ) : authState === 'signup' ? (
                             <>
-                                <p>Continue with social networks</p>
+                                <div className="continueWith">
+                                    <p>Continue with social networks</p>
+                                    <div className="socialIcons">
+                                        <img src={googleIcon} alt=""/>
+                                        <img src={facebookIcon} alt=""/>
+                                        <img src={appleIcon} alt=""/>
+                                    </div>
+                                </div>
                             </>
                         ) : authState === 'login' ? (
                             <>
-                                <div onClick={() => setAuthState('passwordRecovery')}>
+                                <div className={"passwordRecoveryLabel"} onClick={() => setAuthState('passwordRecovery')}>
                                     Forgot Password?
                                 </div>
-                                <p>Continue with social networks</p>
+                                <div className="continueWith">
+                                    <p>Continue with social networks</p>
+                                    <div className="socialIcons">
+                                        <img src={googleIcon} alt=""/>
+                                        <img src={facebookIcon} alt=""/>
+                                        <img src={appleIcon} alt=""/>
+                                    </div>
+                                </div>
                             </>
                         ) : (
                             <>
-                                <span onClick={() => setAuthState('signup')}>Return to Sign Up</span>
+                                <div className={"passwordRecoveryLabel"} onClick={() => setAuthState('signup')}>Return to Sign Up</div>
                             </>
                         )}
-                </p>
             </div>
         </div>
     ) : null;
